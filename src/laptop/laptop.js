927ee0fc034f832e659b623a12ea33100b2f4cc3
Maksim Karelov <max.karelov@gmail.com>
2019-03-15 14:19:56 +0300

    Change video appearing behavior

--- src/laptop/laptop.js
+++ src/laptop/laptop.js
@@ -3,15 +3,7 @@ import { query } from '../base/base';
 import sourceLow from '../../static/video-low.mp4';
 import sourceHigh from '../../static/video-high.mp4';
 
-function insertVideo() {
-  const videoElement = document.createElement('video');
-
-  videoElement.classList.add('laptop_video');
-  videoElement.setAttribute('autoplay', '');
-  videoElement.setAttribute('loop', '');
-  videoElement.setAttribute('muted', '');
-  videoElement.setAttribute('playisinline', '');
-
+function insertSource(videoElement) {
   const sourceElement = document.createElement('source');
 
   sourceElement.setAttribute(
@@ -28,7 +20,23 @@ function insertVideo() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (!navigator.connection || !navigator.connection.saveData) {
-    setTimeout(() => insertVideo(), 1500);
+  if (navigator.connection && navigator.connection.saveData) {
+    return;
   }
+
+  const videoElement = query('.laptop_video');
+  const startTime = Date.now();
+
+  insertSource(videoElement);
+
+  videoElement.addEventListener('canplay', () => {
+    const delta = Date.now() - startTime;
+    const animate = () => query('.laptop_video').classList.add('animated');
+
+    if (delta >= 1500) {
+      animate();
+    } else {
+      setTimeout(animate.bind(this), 1500 - delta);
+    }
+  });
 });
