abf0b25a968bae204e46ca23e38b58dea5714c29
Maksim Karelov <max.karelov@gmail.com>
2019-03-16 14:34:10 +0300

    Add Fathom analytics

---
+++ src/fathom/fathom.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//fathom.karelov.info/tracker.js', 'fathom');
+fathom('set', 'siteId', 'AIONV');
+fathom('trackPageview');
+/* eslint-enable */
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import 'webp-in-css';
+import '/fathom/fathom';
 import '/laptop/laptop';
